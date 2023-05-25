3cbba9e1f4f16b3d4f5007c6c750696f84314587
rmn <t.egawa12164@gmail.com>
2023-05-25 23:37:04 +0900

    isdoneをdateからboolに変更

--- src/app/models.py
+++ src/app/models.py
@@ -5,7 +5,7 @@ class Todo(models.Model):
     title = models.CharField(max_length=100, blank=True)
     description = models.TextField(blank=True)
     date = models.DateField(blank=False)
-    isdone = models.DateField(default=False)
+    isdone = models.BooleanField(default=False)
 
     def __str__(self) -> str:
         return self.title
