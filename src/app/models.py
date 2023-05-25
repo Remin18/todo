c8773709364955480ac025bcc66f009f318959ba
rmn <t.egawa12164@gmail.com>
2023-05-25 23:33:32 +0900

    CRUDまで

--- src/app/models.py
+++ src/app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Todo(models.Model):
+    title = models.CharField(max_length=100, blank=True)
+    description = models.TextField(blank=True)
+    date = models.DateField(blank=False)
+    isdone = models.DateField(default=False)
+
+    def __str__(self) -> str:
+        return self.title
--- src/app/views.py
+++ src/app/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .serializers import *
+from .models import *
 
-# Create your views here.
+# CRUD
+class TodoList(generics.ListAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
+class TodoDetail(generics.RetrieveUpdateAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
+class TodoCreate(generics.CreateAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
+class TodoDelete(generics.DestroyAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
